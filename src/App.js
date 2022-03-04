8667e5f9133e2df7e7e07379be134c8e42ff436f
nhuynh <nhahuynhwrites@gmail.com>
2022-03-03 22:57:09 -0600

    Added changes to App.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
+import { getPlacesData } from "./api";
 
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+
+  useEffect(() => {
+    getPlacesData().then((data) => {
+      console.log(data);
+      setPlaces(data);
+    });
+  }, []);
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const URL = "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
+
+const options = {
+  params: {
+    bl_latitude: "11.847676",
+    tr_latitude: "12.838442",
+    bl_longitude: "109.095887",
+    tr_longitude: "109.149359",
+  },
+  headers: {
+    "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+    "x-rapidapi-key": "d015cd2587msh5b0b597a398b8d2p1f48c6jsn763e8889cb01",
+  },
+};
+
+export const getPlacesData = async () => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(URL, options);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
